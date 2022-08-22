ee7f54644d9de33e470da5b3eec7fa414644f132
aaronlyy <aaronlevican@gmail.com>
2022-08-22 16:42:09 +0200

    added animations

--- short.krotesq.com/src/components/Input/index.jsx
+++ short.krotesq.com/src/components/Input/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './index.css'
+import { motion } from 'framer-motion'
 
 const Input = () => {
   return (
     <div className='Input'>
         <form action="">
             <input type="text" name="url" id="url" placeholder='Enter a link..' />
-            <button>Paste from clipboard</button>
-            <input type="button" value="GO!" />
+            <motion.button whileHover={{scale: 1.02, cursor: 'pointer'}} whileTap={{scale: 0.98}}>Paste from clipboard</motion.button>
+            <motion.input type="button" value="GO!" whileHover={{scale: 1.02, cursor: 'pointer'}} whileTap={{scale: 0.98}}/>
         </form>
     </div>
   )
