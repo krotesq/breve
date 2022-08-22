ffb4a421f888ac83df61c910994da559d116ef16
aaronlyy <aaronlevican@gmail.com>
2022-08-22 15:25:43 +0200

    added header

--- short.krotesq.com/src/components/App/index.jsx
+++ short.krotesq.com/src/components/App/index.jsx
@@ -1,6 +1,15 @@
+import Header from '../Header'
+import Shortener from '../Shortener'
+import Footer from '../Footer'
+import './index.css'
+
 function App() {
   return (
-    <h1>servus</h1>
+    <div className="App">
+      <Header/>
+      <Shortener/>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ short.krotesq.com/src/components/Shortener/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shortener = () => {
+  return (
+    <div>Shortener</div>
+  )
+}
+
+export default Shortener
