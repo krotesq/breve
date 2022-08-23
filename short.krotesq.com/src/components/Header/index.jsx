b10e1c9c65331bffacfe8532ef17c7d4a0bd829b
aaronlyy <aaronlevican@gmail.com>
2022-08-23 12:50:28 +0200

    width fix

--- short.krotesq.com/src/components/Header/index.jsx
+++ short.krotesq.com/src/components/Header/index.jsx
@@ -5,10 +5,9 @@ import { motion } from 'framer-motion'
 const Header = () => {
   return (
     <motion.div className='Header' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1}}>
-        <h1>KROTESQ URL SHORTENER</h1>
+        <h1>KROTESQ URL ShORTENER</h1>
         <ul>
             <li><a href="#">BACK TO KROTESQ</a></li>
-            <li><a href="#">REPORT A PROBLEM</a></li>
         </ul>
     </motion.div>
   )
