c44679c364bf30e9ea7a8cfc64b711d9db5a947b
aaronlyy <aaronlevican@gmail.com>
2022-08-23 13:18:48 +0200

    ye

--- short.krotesq.com/src/components/Header/index.jsx
+++ short.krotesq.com/src/components/Header/index.jsx
@@ -5,7 +5,8 @@ import { motion } from 'framer-motion'
 const Header = () => {
   return (
     <motion.div className='Header' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1}}>
-        <h1>KRTSQ</h1>
+        <h1>BREVE</h1>
+        <h2>A KROTESQ LINK SHORTENER</h2>
         <ul>
           <li><a href="https://krotesq.com">BACK TO KROTESQ.COM</a></li>
         </ul>
