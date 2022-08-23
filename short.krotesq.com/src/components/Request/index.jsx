ed08a5f2658d686f6de9ebe386e57dd91b3e1d0a
aaronlyy <aaronlevican@gmail.com>
2022-08-23 14:20:22 +0200

    added navigate to request if no data is passed

--- short.krotesq.com/src/components/Request/index.jsx
+++ short.krotesq.com/src/components/Request/index.jsx
@@ -1,24 +1,22 @@
 import {React, useEffect} from 'react'
 import {useLocation, useNavigate} from 'react-router-dom'
 import { motion } from 'framer-motion'
-import axios from 'axios'
 import Loading from '../Loading'
 
 const Request = () => {
-  const {state} = useLocation();
+  const location = useLocation();
   const navigate = useNavigate();
-  const {url} = state;
+  const data = location.state;
 
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   useEffect(() => {
-    // axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
-    //   console.log(res.data);
-    //   const shortUrl = res.data;
-    //   
-    // })
+    if (data == null) {
+      navigate('/');
+      return;
+    }
     sleep(2000).then(() => {
       navigate('/result', {state: {shortUrl: 'tesq.io/t3s65'}});
     })
