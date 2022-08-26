87a433385aab6e5090bc83550989000570ec4182
aaronlyy <aaronlevican@gmail.com>
2022-08-26 13:53:33 +0200

    .

--- breve/src/components/Result/index.jsx
+++ breve/src/components/Result/index.jsx
@@ -1,6 +1,6 @@
 import {React, useEffect, useState} from 'react'
 import {motion} from 'framer-motion'
-import { useLocation, useNavigate} from 'react-router-dom'
+import { useLocation} from 'react-router-dom'
 import './index.css'
 
 const Result = () => {
@@ -10,28 +10,19 @@ const Result = () => {
   const [response, setResponse] = useState({});
 
   useEffect(() => {
-
-    // check if data from input is passed
-
-    if (data == null) {
-      setResponse('n/a')
-      return;
-    }
-
-    fetch("https://jsonplaceholder.typicode.com/posts", {
+  fetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
       method: 'POST',
-      body: JSON.stringify({
-        url: data.url
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-      }
-    })
-    .then(res => res.json())
-    .then(res => {
-      setResponse(res);
-    })
+      body: JSON.stringify({url: data.url}),
+      headers: {'Content-type': 'application/json; charset=UTF-8'}
+    }
+  )
+  .then(res => res.json())
+  .then(res => {
+    setResponse(res);
   })
+})
 
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1}}>
