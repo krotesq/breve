6a402089d47d363677d7fe855fbf72cb5387399e
aaronlyy <aaronlevican@gmail.com>
2022-09-01 15:39:41 +0200

    added function to handle form change

--- breve/src/components/Request/index.jsx
+++ breve/src/components/Request/index.jsx
@@ -1,17 +1,45 @@
 import './index.css'
 
-import React from 'react'
+import isURL from 'validator/lib/isURL'
+
+import { useState } from 'react'
 
 const Request = () => {
-  return (
-    <div className='Request'>
-        <form className='Request__form'>
-            <input type="text" id='txtLongUrl' name='longUrl'/>
-            <input type="button" id='btnPaste' value='Paste URL from clipboard'/>
-            <input type="submit" id='btnSubmit' value='Submit'/>
-        </form>
-    </div>
-  )
+
+    const [longUrl, setLongUrl] = useState('');
+    const [customCode, setCustomCode] = useState('');
+
+    const handleChangeUrl = (e) => {
+        setLongUrl(e.target.value);
+    }
+
+    const handleChangeCode = (e) => {
+        setCustomCode(e.target.value);
+    }
+
+    const handleClickPaste = async (e) => {
+        e.preventDefault();
+        setLongUrl(await navigator.clipboard.readText());
+    }
+
+    const handleClickSubmit = async (e) => {
+        e.preventDefault();
+        // validate if longUrl is a valid URL
+        if (!isURL(longUrl)) {
+            return;
+        }
+    }
+
+    return (
+        <div className='Request'>
+            <form className='Request__form'>
+                <input type="text" id='txtLongUrl' name='longUrl' placeholder='Enter a link...' value={longUrl} onChange={handleChangeUrl}/>
+                <input type="text" id='txtCustomCode' name='customCode' placeholder='Enter a custom code' value={customCode} onChange={handleChangeCode}/>
+                <input type="button" id='btnPaste' value='Paste URL from clipboard' onClick={handleClickPaste}/>
+                <input type="submit" id='btnSubmit' value='Submit' onClick={handleClickSubmit}/>
+            </form>
+        </div>
+    )
 }
 
 export default Request
