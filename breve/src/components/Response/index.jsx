e035b01bccd3b4a030f435988cf2c0b1d213f424
aaronlyy <aaronlevican@gmail.com>
2022-09-01 15:59:49 +0200

    response handlers

--- breve/src/components/Response/index.jsx
+++ breve/src/components/Response/index.jsx
@@ -1,18 +1,33 @@
 import './index.css'
 
-import React from 'react'
+import {useLocation, useNavigate} from 'react-router-dom'
 
 const Response = () => {
-  return (
-    <div className='Response'>
-        <form className='Response__form'>
-            <input type="text" id='txtShortUrl' placeholder='No short URL available' readOnly />
-            <input type="submit" id='btnCopy' value='Copy URL to clipboard'/>
-            <input type="button" id='btnNew' value='Short another link' />
-        </form>
-        
-    </div>
-  )
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const data = location.state;
+
+    const handleClickCopy = (e) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(data.shortUrl);
+      }
+
+      const handleClickNew = (e) => {
+        e.preventDefault();
+        navigate('/');
+      }
+    
+
+    return (
+        <div className='Response'>
+            <form className='Response__form'>
+                <input type="text" id='txtShortUrl' placeholder='No short URL available' readOnly value={data.shortUrl} />
+                <input type="button" id='btnCopy' value='Copy URL to clipboard' onClick={handleClickCopy}/>
+                <input type="button" id='btnNew' value='Short another link' onClick={handleClickNew}/>
+            </form>
+        </div>
+    )
 }
 
 export default Response
